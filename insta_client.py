4a07cb79ba8380f9a85545b597e7a63e36f8bd16
courageousstrawberry <elech2a@gmail.com>
2024-09-25 16:11:46 -0700

    create follow bot

--- insta_client.py
+++ insta_client.py
@@ -12,3 +12,6 @@ medias = client.hashtag_medias_recent(hashtag, 20)
 for i, media in enumerate(medias):
     client.media_like(media.id)
     print(f"Liked post number {i+1} of hashtag {hashtag}")
+    if i%5 == 0:
+        client.user_follow(media.user.pk)
+        print(f"Followed user {media.user.username}")
