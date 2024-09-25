0dd2edb43ae3d6b20ea56a71523998ede74f000a
courageousstrawberry <elech2a@gmail.com>
2024-09-25 15:55:20 -0700

    add looping through various ig hashtags

--- instagrapi.py
+++ instagrapi.py
@@ -3,4 +3,8 @@ from instagrapi import Client
 username, password = "courageousstrawberry", "elenibetty"
 
 client = Client()
-client.login()
+client.login(username, password)
+
+hashtag = "programming"
+
+medias = client.hashtag_medias_recent(hashtag, 20)
