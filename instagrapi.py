05d9c685db9f5d4d43b612a8088d3678022aae02
courageousstrawberry <elech2a@gmail.com>
2024-09-25 15:57:50 -0700

    create a like bot

--- instagrapi.py
+++ instagrapi.py
@@ -8,3 +8,7 @@ client.login(username, password)
 hashtag = "programming"
 
 medias = client.hashtag_medias_recent(hashtag, 20)
+
+for i, media in enumerate(medias):
+    client.media_like(media.id)
+    print(f"Liked post number {i+1} of hashtag {hashtag}")
